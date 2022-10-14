678de31cf3205f55c403351d217939948ce93edd
Jonas Marques <jonas.marques2021.jm5@gmail.com>
2022-10-14 19:05:08 -0300

    <feat>(<front-end>): <eventos>

---
+++ src/Components/Eventos/Eventos.style.js
@@ -0,0 +1,12 @@
+import styled from "@emotion/styled"
+
+export const Wrapper = styled.div`
+
+.ImageContainner{
+    position: relative;
+    .imgText{
+        position: absolute;
+        top: 85%;
+    }
+}
+`
